e3888080b70a51d70294a681f1b4006c2cdf6780
unknown <pawan.sviet@gmail.com>
2022-09-22 14:37:57 +0530

    modified return method

--- 03_Mini_Library.py
+++ 03_Mini_Library.py
@@ -1,11 +1,19 @@
+
+
+
 class Library:
-    available_books = ["ml", "python", "DSA", "sql"]
+
+    # library_file_read = open("Library.txt", "r")
+    # library_file_write = open("Library.txt", "w")
+    
+
     def borrowBook(self):
         book_name = input("[Borrow] Please enter book name: ")
 
         if book_name not in self.available_books:
             print("**Please enter a valid book name** \n Press 'q' to quit")
             if book_name == "q": 
+                self.library_file.close()
                 return None
             else:
                 print("Available books are: ",self.available_books)
@@ -14,18 +22,40 @@ class Library:
         else:
             self.available_books.remove(book_name)
             print("Available books are: ",self.available_books)
+            self.library_file.close()
         
 
     def returnBook(self):
-        book_name = input("Enter book name: ")
-        self.available_books.append(book_name)
-        print("Available books are: ",self.available_books)
-        
+        library_file_read = open("Library.txt","r")
+        library = library_file_read.readlines()
+
+        return_book = input("Enter book name: ")
+        return_book = return_book + "\n"
+
+        if return_book not in library:
+            
+            return_book_to_library = open("Library.txt","a+")
+            return_book_to_library.writelines([return_book])
+            library_file_read.close()
+            return_book_to_library.close()
+
+            Library.availableBooks(self)
+            
+        else:
+            print("***Already returned***")
 
     def donateBook(self):
         book_name = input("Please enter the book name: ")
-        self.available_books.append(book_name)
-        print("Available books are: ",self.available_books)
+        library_file_append = open("Library.txt","a+")
+        library_file_append.writelines([book_name, "\n"])
+        library_file_append.close()
+
+        Library.availableBooks(self)
+
+    def availableBooks(self):
+        all_books = open("Library.txt","r")
+        books = all_books.readlines()
+        print("Available Books: ",books)
         
 
 
