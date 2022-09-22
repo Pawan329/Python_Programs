09134bf0871fca146a11bb5e60835bd7b8f68e96
unknown <pawan.sviet@gmail.com>
2022-09-22 11:24:31 +0530

    added return and donate book method

--- 03_Mini_Library.py
+++ 03_Mini_Library.py
@@ -1,29 +1,45 @@
 class Library:
-
-    available_books = ["machine learning", "sql", "python", "docker"]
-
+    available_books = ["ml", "python", "DSA", "sql"]
     def borrowBook(self):
         book_name = input("[Borrow] Please enter book name: ")
-        
+
         if book_name not in self.available_books:
             print("**Please enter a valid book name** \n Press 'q' to quit")
             if book_name == "q": 
                 return None
             else:
+                print("Available books are: ",self.available_books)
                 Library.borrowBook(self)
 
         else:
             self.available_books.remove(book_name)
+            print("Available books are: ",self.available_books)
         
 
     def returnBook(self):
-        pass
+        book_name = input("Enter book name: ")
+        self.available_books.append(book_name)
+        print("Available books are: ",self.available_books)
+        
 
     def donateBook(self):
-        pass
+        book_name = input("Please enter the book name: ")
+        self.available_books.append(book_name)
+        print("Available books are: ",self.available_books)
+        
 
 
 action = Library()
 
-action.borrowBook()
-print("Available books: ",action.available_books)
+user_input = int(input("Select: \n1. To Borrow book \n2. To return book \n3. To Donate book \n4. To see list of available books\n"))
+if user_input == 1:
+    action.borrowBook()
+
+elif user_input == 2:
+    action.returnBook()
+
+elif user_input == 3:
+    action.donateBook()
+
+elif user_input == 4:
+    print(action.available_books)
